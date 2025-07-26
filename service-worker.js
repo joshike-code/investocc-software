d0d3c70bc364bca8ab2cc28af1b6243bfe4841c8
Ikenna Anunuso <joshikenna@gmail.com>
2025-07-26 21:22:14 +0100

    service worker update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'investocc-v1.0.3';
+const CACHE_NAME = 'investocc-v1.0.4';
 const ASSETS_TO_CACHE = [
     '/assets/images/logo/logo-icon.png',
     '/assets/images/logo/app-icon.png',
